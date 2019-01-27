57b9021f4ac9f94228adc983cca509d3923c363b
Ebrima Tunkara <etunkara51@gmail.com>
2019-01-27 00:01:37 -0500

    Iniital Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columnDefs: [
+                {headerName: 'Name', field: 'name'},
+                {headerName: 'Position', field: 'position'},
+                {headerName: 'Overall Rank', field: 'rankOverall'},
+                {headerName: 'Position Rank', field: 'rankPos'},
+                {headerName: 'Team', field: 'team'}
+            ],
+            rowData: [],
+            api: {}
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <input
+                        type="text"
+                    />
+                </div>
+                <div
+                    className="ag-theme-balham"
+                    style={{height: '900px', width: '900px'}}
+                >
+                    <AgGridReact
+                        pagination={true}
+                        sortable={true}
+                        filter={true}
+                        columnDefs={this.state.columnDefs}
+                        rowData={this.state.rowData}
+                    >
+                    </AgGridReact>
+                </div>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/players/rankings")
+            .then(result => result.json())
+            .then(rowData => this.setState({rowData}))
+    }
+
+
 }
+
 
 export default App;
